964d36b9078de9cd05e8172f325c3ea8557fac7f
unknown <jose.oreo99@gmail.com>
2019-10-03 19:51:09 -0600

    Primer proceso de descencriptar zig zag

--- Laboratorio2/Laboratorio2/Cifrado/CifradoZig_Zag.cs
+++ Laboratorio2/Laboratorio2/Cifrado/CifradoZig_Zag.cs
@@ -90,5 +90,66 @@ namespace Laboratorio2.Cifrado
                 }
             }
         }
+
+
+        public void Descifrar(string fileName, string path, string FilePath, int clave)
+        {
+            var pathDes = Path.Combine(FilePath, Path.GetFileNameWithoutExtension(fileName) + "DescifradoZ.txt");
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    var totalC = reader.BaseStream.Length;
+                    double m = (2 * (Convert.ToDouble(clave)) + Convert.ToDouble(totalC) - 3) / (2 * (Convert.ToDouble(clave)) - 2);
+                    var arriba = Convert.ToInt32(Math.Ceiling(m));
+                    var abajo = arriba - 1;
+                    var cantIntermedios = clave - 2;
+                    var intermedios = 2 * (abajo);
+                    var filas = new string[clave];
+                    var todoIntermedio = "";
+                    var byteBuffer = new char[bufferLength];
+                    var x = 0;
+                    var textfromfile = new List<string>();
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        byteBuffer = reader.ReadChars(bufferLength);
+                        foreach (var item in byteBuffer)
+                        {
+                            textfromfile.Add(item.ToString());
+                        }
+                    }
+                    //Incerta los primeros caracteres a la primer fila
+                    for(int i = 0; i < arriba; i++)
+                    {
+                        filas[0] += textfromfile[i];
+                    }
+                    //Inserta los ultimos caracteres a la ultima fila
+                    for (int i = textfromfile.Count-abajo; i < textfromfile.Count; i++)
+                    {
+                        filas[clave-1] += textfromfile[i];
+                    }
+                    //Agarro todos los que no estan ni en la primera ni en la ultima
+                    for(int i = arriba; i < textfromfile.Count - abajo; i++)
+                    {
+                        todoIntermedio += textfromfile[i];
+                    }
+                    //Pregunto
+                    if (todoIntermedio.Length > intermedios)
+                    {
+                        //si es mayor a la cantidad max de char en los intermedios entonces divide
+                        var division = todoIntermedio.Length / cantIntermedios;
+                        //Aca hay que buscar una manera de separar el texto para llenar las filas
+                        //Primera division:
+                        for (int i = 0; i < cantIntermedios; i++)
+                        {
+                            filas[clave - cantIntermedios - 1] += todoIntermedio[i];
+                        }
+                    }
+
+                    //Esto es solo para poner breakpoint y ver si los mete bien al vector
+                    var prueba = filas;
+                }
+            }
+        }
     }
 }
--- Laboratorio2/Laboratorio2/Controllers/Zig_ZagController.cs
+++ Laboratorio2/Laboratorio2/Controllers/Zig_ZagController.cs
@@ -67,5 +67,51 @@ namespace Laboratorio2.Controllers
                 return RedirectToAction(nameof(ArchivoCifrado));
             }
         }
+
+
+
+
+        public ActionResult ClaveDes(string fileName, string niveles)
+        {
+            Zig_ZagViewModel zigzag = new Zig_ZagViewModel();
+            zigzag.NombreDelArchivo = fileName;
+            zigzag.NivelesDeSeparacion = Convert.ToInt32(niveles);
+            return View(zigzag);
+        }
+
+        // POST: Zig_Zag/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ClaveDes(Zig_ZagViewModel zigzag)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    if (zigzag.NivelesDeSeparacion > 0)
+                    {
+                        //Mandar a llamar al metodo para cifrar
+                        var path = Path.Combine(Server.MapPath("~/Archivo"), zigzag.NombreDelArchivo);
+                        FilePath = Server.MapPath("~/Archivo");
+                        CifradoZig_Zag zig = new CifradoZig_Zag();
+                        zig.Descifrar(zigzag.NombreDelArchivo, path, FilePath, zigzag.NivelesDeSeparacion);
+                        return RedirectToAction(nameof(ArchivoCifrado));
+                    }
+                    else
+                    {
+                        return View(zigzag);
+                    }
+                }
+                else
+                {
+                    return View(zigzag);
+                }
+            }
+            catch
+            {
+                return RedirectToAction(nameof(ArchivoCifrado));
+            }
+        }
     }
 }
