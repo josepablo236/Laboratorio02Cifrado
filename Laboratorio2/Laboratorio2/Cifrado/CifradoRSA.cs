ac8608418c85c3e7219c642cb9c6e8e366fcc8d6
Lisbeth Diaz <lisbeth.diaz8@gmail.com>
2019-10-23 07:40:33 -0600

    CreacionLlaves
    Creacioón de llaves publica y privada

--- Laboratorio2/Laboratorio2/Cifrado/CifradoRSA.cs
+++ Laboratorio2/Laboratorio2/Cifrado/CifradoRSA.cs
@@ -9,7 +9,47 @@ namespace Laboratorio2.Cifrado
     {
         public void GenerarLlaves(int numero1, int numero2, string FilePath)
         {
-
+            var p = numero1;
+            var q = numero2;
+            //n
+            var n = p * q;
+            //Calcular Q(n)
+            var QN = (p - 1) * (q - 1);
+            //calcular e
+            int e = 2;int count;
+            while (e < QN)
+            {
+                count = MCD(e, n);
+                if (count == 1)
+                    break;
+                else
+                    e++;
+            }
+            
+            //Calcular d
+            var d = 1;
+            var tempo = 0;
+            do
+            {
+                d++;
+                tempo = (d * e) % n;
+            } while (tempo != 1);
+            d += n;
+           
+        }
+        public int MCD(int a, int h)
+        {
+            int temp=0;
+            while (temp != 1)
+            {
+                temp = a % h;
+                if (temp == 0)
+                    return h;
+                a = h;
+                h = temp;
+            }
+            return 0;
         }
     }
+    
 }
