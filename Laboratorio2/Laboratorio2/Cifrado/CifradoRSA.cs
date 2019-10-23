e3ed91a756f124347cff195e6f4564534d3a464e
unknown <jose.oreo99@gmail.com>
2019-10-22 22:41:05 -0600

    Creacion de las visatas para cifrado RSA

---
+++ Laboratorio2/Laboratorio2/Cifrado/CifradoRSA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.Cifrado
+{
+    public class CifradoRSA
+    {
+        public void GenerarLlaves(int numero1, int numero2, string FilePath)
+        {
+
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio2/Models/RSAViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio2.Models
+{
+    public class RSAViewModel
+    {
+        public int ValorP { get; set; }
+        public int ValorQ { get; set; }
+    }
+}
